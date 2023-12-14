482dad9b62e9b743cb97ea3b09a7775df21c1ded
Steven Mattmann <steven.mattmann07@gmail.com>
2023-12-14 09:33:38 +0100

    9.00

---
+++ 04_Reisedaurem_zum_Mond_Aufgabe/Mondreise.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+namespace Reisedauer_zum_Mond_Aufgabe
+{
+    public class Mondreise
+    {
+        double speedKmh;
+        int distanzmond = 384400;
+
+        public Mondreise(double speed)
+        {
+            speedKmh = speed;
+        }
+
+        public double BerechnenTage()
+        {
+            double result = distanzmond / speedKmh / 24;
+            return result;
+        }
+
+        public double BerechnenStunden()
+        {
+            double result = distanzmond / speedKmh;
+            return result;
+        }
+    }
+}
